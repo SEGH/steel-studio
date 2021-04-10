1d7d20d15ae9acb3c0f33ee65082b3f752194a2a
Susan Holland <segh@fastmail.com>
2021-04-10 13:59:03 -0400

    Add snipcart-orders plugin

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -3,5 +3,13 @@ export default {
     {
       name: 'project-users'
     },
+    {
+      name: 'snipcart-orders',
+      options: 
+      {
+        apiKey: "ST_MzMyZjlhYzktNDMzOS00ZGU3LWI2Y2QtZjJmODVlNzIzMmI1NjM3NTM2NTk2OTM2NDg1NTg1", // SNIPCART SECRET API KEY
+        limit: 5, //DEFAULTS TO FIVE
+      }
+    }
   ]
 }
