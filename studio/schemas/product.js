9748dae4d1d1dd4c1115e879c33797120f1a05ae
Susan Holland <segh@fastmail.com>
2021-04-22 17:57:47 -0400

    Add price field to product

--- studio/schemas/product.js
+++ studio/schemas/product.js
@@ -20,6 +20,12 @@ export default {
       }
     },
     {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      validation: Rule => Rule.required(),
+    },
+    {
       title: 'Variants',
       name: 'variants',
       type: 'array',
